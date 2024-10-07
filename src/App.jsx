8ff1de3cd80dbedff9b9f3562df984809020f302
Yuriy Kuzin <yura_kuzin_03@mail.ru>
2024-10-07 13:07:34 +0500

    Initial commit, header

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header/Header.jsx";
+
+function App() {
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            Resume creator
+        </header>
+    );
+};
+
+export default Header;
