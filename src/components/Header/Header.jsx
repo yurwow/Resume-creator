b2a1af7cf29a825d34821360d56e010812a71dbe
Yuriy Kuzin <yura_kuzin_03@mail.ru>
2024-10-08 23:43:42 +0500

    Добавил анимацию в header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import styles from './Header.module.css';
 
-
 const Header = () => {
     return (
         <header className={styles.header}>
-            <h1>Resume creator</h1>
+            <div className={styles.animation}>RESUME</div>
+            <div style={{marginLeft:'5px'}} className={styles.animation}>
+                <span className={styles.animation_span}>CREATER CONTENT</span>
+            </div>
         </header>
     );
 };
